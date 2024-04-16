dfba0f002e29fec86342d28c594b752db22d227c
Ellen Coutinho <ellencls@al.insper.edu.br>
2024-04-15 22:21:51 -0300

    Botões análogos - arquivo Python

--- main/main.py
+++ main/main.py
@@ -9,6 +9,19 @@ def compara_string(comando,string):
 while True:
     data = ser.read(3)
     print("data: ", data)
-    if(compara_string(data,'ENT')):
+    if compara_string(data,'ENT'):
         pyautogui.press('enter')
+    elif compara_string(data, 'ESC'):
+        pyautogui.press('esc')
+    elif compara_string(data, 'GED'):
+        pyautogui.keyDown('shiftleft')
+    elif compara_string(data, 'GES'):
+        pyautogui.keyUp('shiftleft')
+    elif compara_string(data, 'GDD'):
+        pyautogui.keyDown('shiftright')
+    elif compara_string(data, 'GDS'):
+        pyautogui.keyUp('shiftright')
+    elif compara_string(data, 'VIS'):
+        pyautogui.press('c')
+
     
